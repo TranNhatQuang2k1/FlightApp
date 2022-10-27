5e2f54af64a196e5db11c4ee8776bf70509b2b6b
ngdangkiet <kietnguyen17052001@gmail.com>
2022-10-27 23:01:34 +0700

    add root register view

--- flightapp/flight/urls.py
+++ flightapp/flight/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
-    path("register", views.register_view, name="register"),
+    path("register", views.register_view, name="register"), # url register view
     path("logout", views.logout_view, name="logout"),
 ]
